283ee5a33e4d8ef2b7e3a3f00e25dc06d277d5c1
Ahmed Wagdy <ahmed.w.amin@gmail.com>
2018-11-05 12:31:33 +0200

    remove staff member required

--- ckeditor_uploader/urls.py
+++ ckeditor_uploader/urls.py
@@ -9,13 +9,13 @@ from . import views
 
 if django.VERSION >= (1, 8):
     urlpatterns = [
-        url(r'^upload/', staff_member_required(views.upload), name='ckeditor_upload'),
-        url(r'^browse/', never_cache(staff_member_required(views.browse)), name='ckeditor_browse'),
+        url(r'^upload/', (views.upload), name='ckeditor_upload'),
+        url(r'^browse/', never_cache((views.browse)), name='ckeditor_browse'),
     ]
 else:
     from django.conf.urls import patterns
     urlpatterns = patterns(
         '',
-        url(r'^upload/', staff_member_required(views.upload), name='ckeditor_upload'),
-        url(r'^browse/', never_cache(staff_member_required(views.browse)), name='ckeditor_browse'),
+        url(r'^upload/', (views.upload), name='ckeditor_upload'),
+        url(r'^browse/', never_cache((views.browse)), name='ckeditor_browse'),
     )
